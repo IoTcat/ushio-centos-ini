bab22c2fa6967c6603c36a518e881fae31945e91
呓喵酱 <i@iotcat.me>
2020-02-05 15:01:39 +0000

    Update iptables.sh

--- utilities/iptables.sh
+++ utilities/iptables.sh
@@ -6,3 +6,6 @@ sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
 yum install iptables-services iptables-devel -y
 systemctl start iptables
 systemctl enable iptables
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
+service iptables save
